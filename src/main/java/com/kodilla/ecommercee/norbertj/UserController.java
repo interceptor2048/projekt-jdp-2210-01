c8107c118fba7654a8df9ac3135e1b01c92d237f
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-09-22 15:01:43 +0200

    task JDP221001-5, also creation of UserDto class according users.spec.json

---
+++ src/main/java/com/kodilla/ecommercee/norbertj/UserController.java
@@ -0,0 +1,30 @@
+package com.kodilla.ecommercee.norbertj;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/users")
+@AllArgsConstructor
+public class UserController {
+
+    @PostMapping("/create_user")
+    public ResponseEntity<UserDto> createUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String address,
+                                              @RequestParam String email, @RequestParam String login, @RequestParam String password){
+        return ResponseEntity.ok(new UserDto(1,login,1,666));
+    }
+
+    @PutMapping("/block_user")
+    public ResponseEntity<String> blockUserById(@RequestParam int id){
+        return ResponseEntity.ok("User with requested ID was blocked");
+    }
+
+    @GetMapping("/get_key")
+    public ResponseEntity<Integer> getUserKey(@RequestParam String login,@RequestParam String password){
+        return ResponseEntity.ok(1);
+    }
+
+}
