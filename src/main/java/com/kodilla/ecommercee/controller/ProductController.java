0bf9a6a9b23ccc776703ce86ec6d1744b36f2a83
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-09-29 15:10:31 +0200

    structure fixed ProductEntity name change and relations with other entities

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,5 +1,6 @@
-package com.kodilla.ecommercee.asia;
+package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
---
+++ src/main/java/com/kodilla/ecommercee/domain/entity/ProductEntity.java
@@ -0,0 +1,41 @@
+package com.kodilla.ecommercee.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "Product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id", unique = true)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_description")
+    private String description;
+
+    @Column(name = "product_price")
+    private Double price;
+
+  //  @ManyToOne
+  //  @JoinColumn(name = "product_group_id", referencedColumnName = "product_group_id")
+//    @JsonBackReference
+//    private ProductGroupEntity productGroupId;
+
+ //   @OneToMany(mappedBy = "productId")
+ //   @JsonManagedReference
+ //   private List<ProductsInCartEntity> productsInCart;
+
+}
