9375a0e2feb3e665227508882074c9abd4435e8b
antykom <36765862+antykom@users.noreply.github.com>
2022-10-03 20:57:13 +0200

    Task JDP221001-17 added

--- src/main/java/com/kodilla/ecommercee/domain/dto/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/CartDto.java
@@ -1,9 +1,12 @@
 package com.kodilla.ecommercee.domain.dto;
 
-import com.kodilla.ecommercee.domain.dto.ProductDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class CartDto {
     private Long id;
     private Long userId;
