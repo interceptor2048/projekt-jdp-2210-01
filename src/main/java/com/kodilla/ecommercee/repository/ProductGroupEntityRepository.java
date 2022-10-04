01890bb27fb4d7b7bdef4e6977dbb7b28ed01e58
Krzysiek Zuczkowski <krzysiek23@windowslive.com>
2022-10-04 23:06:14 +0200

    ProductGroupEntityRepository annotation

--- src/main/java/com/kodilla/ecommercee/repository/ProductGroupEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductGroupEntityRepository.java
@@ -2,9 +2,13 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.entity.ProductGroupEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
+@Repository
 public interface ProductGroupEntityRepository extends CrudRepository<ProductGroupEntity, Long> {
 
     List<ProductGroupEntity> findByGroupName(String groupName);
