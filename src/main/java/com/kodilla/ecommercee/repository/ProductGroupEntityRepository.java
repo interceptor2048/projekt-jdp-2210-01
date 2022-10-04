060a28e4b3e695ff1d979d18c1ce3a5098725a98
Krzysiek Zuczkowski <krzysiek23@windowslive.com>
2022-10-04 22:50:45 +0200

    testProductGroupEntityFindByGroupName

--- src/main/java/com/kodilla/ecommercee/repository/ProductGroupEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductGroupEntityRepository.java
@@ -3,6 +3,11 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.entity.ProductGroupEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductGroupEntityRepository extends CrudRepository<ProductGroupEntity, Long> {
 
+    List<ProductGroupEntity> findByGroupName(String groupName);
+
+    void deleteByGroupName(String groupName);
 }
--- src/test/java/com/kodilla/ecommercee/domain/entity/ProductGroupEntityTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/entity/ProductGroupEntityTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 @SpringBootTest
 public class ProductGroupEntityTest {
@@ -16,10 +17,13 @@ public class ProductGroupEntityTest {
     @Autowired
     private ProductGroupEntityRepository repository;
 
+    private static final String GROUP_NAME = "Test: name";
+    private static final String GROUP_TYPE = "Test: type";
+
     @Test
     void testProductGroupEntitySave() {
         //Given
-        ProductGroupEntity productGroup = new ProductGroupEntity("name", "type");
+        ProductGroupEntity productGroup = new ProductGroupEntity(GROUP_NAME, GROUP_TYPE);
 
         //Wehn
         repository.save(productGroup);
@@ -33,5 +37,24 @@ public class ProductGroupEntityTest {
         repository.deleteById(id);
     }
 
+    @Test
+    void testProductGroupEntityFindByGroupName() {
+        //Given
+        ProductGroupEntity productGroup = null;//new ProductGroupEntity(GROUP_NAME, GROUP_TYPE);
+        for(int i = 0; i < 3; i++) {
+            productGroup = new ProductGroupEntity(GROUP_NAME, GROUP_TYPE);
+            repository.save(productGroup);
+        }
 
+        String groupName = productGroup.getGroupName();
+
+        //When
+        List<ProductGroupEntity> readProductsGroup = repository.findByGroupName(groupName);
+
+        //Then
+        assertEquals(3, readProductsGroup.size());
+
+        //CleanUp
+        repository.deleteByGroupName(groupName);
+    }
 }
